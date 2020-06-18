d8e71efa6c086a28ad6eef6658143aff419af8ec
Joe Zou <shaofa@hotmail.com>
2020-06-18 14:24:36 +1000

    Add project files.

---
+++ imyb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace imyb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
